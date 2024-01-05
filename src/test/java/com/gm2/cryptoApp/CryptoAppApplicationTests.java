d0ac6b022684f125eeb0088696417f78fbd98ef8
crozetta <marcelocrozetta@gmail.com>
2024-01-05 10:47:12 -0300

    commit

---
+++ src/test/java/com/gm2/cryptoApp/CryptoAppApplicationTests.java
@@ -0,0 +1,14 @@
+package com.gm2.cryptoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoAppApplicationTests {
+
+
+	@Test
+	void contextLoads() {
+	}
+
+}
